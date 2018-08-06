4090e5a9871ada5ce40475e60dcfc76184b39681
nrck <challengerk.8000@gmail.com>
2018-08-07 01:58:57 +0900

    jobnetAPIの処理変更

--- api/jobnet.api.js
+++ api/jobnet.api.js
@@ -1,30 +1,33 @@
 'use strict';
 (function () {
+    const cmCallback = function (err, data) {
+        if (err !== undefined || data === undefined) {
+            response.status(500);
+            response.end();
+            return;
+        }
+        response.status(200);
+        response.header("Content-Type", "application/json; charset=utf-8");
+        response.end(JSON.stringify(newJobnet, undefined, '  '));
+        return;
+    };
+
+    const jobnetName = request.query.jobnetName || undefined;
+    const newJobnet = request.query.jobnet;
+
     switch (request.method) {
         case 'POST':
-            let newJobnet = request.query;
-            if (typeof newJobnet === 'string') {
-                try {
-                    newJobnet = JSON.parse(newJobnet);
-                } catch (error) {
-                    response.sendStatus(400);
-                    response.end(error.message);
-
-                    return;
-                }
-            }
-
-            if (JSON.stringify(newJobnet) === '{}') {
-                response.sendStatus(400);
-
-                return;
+            // 新規作成・更新
+            if (jobnetName === undefined) {
+                clientManager.putDefineJobnet(newJobnet, cmCallback);
             } else {
-                jobnet.define.push(newJobnet);
-                response.status(200);
-                response.header("Content-Type", "application/json; charset=utf-8");
-                response.end(JSON.stringify(newJobnet, undefined, '  '));
+                clientManager.updateDefineJobnet(jobnetName, newJobnet, cmCallback);
             }
+            return;
 
+        case 'DELETE':
+            // 削除
+            clientManager.removeDefineJobnet(jobnetName, cmCallback);
             return;
 
         default:
@@ -36,7 +39,6 @@
             response.status(200);
             response.header("Content-Type", "application/json; charset=utf-8");
             response.end(JSON.stringify(jobnet, undefined, '  '));
-
             return;
     }
 })();
